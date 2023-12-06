d52830262d38939da8595c16d96c99484ce5135c
lajor <rojal.lajor@gmail.com>
2023-12-06 14:48:53 +0545

    sign up card change

--- lib/widgets/login/login_card.dart
+++ lib/widgets/login/login_card.dart
@@ -1,21 +1,27 @@
 import 'package:ecom/widgets/login/my_text_field.dart';
 import 'package:flutter/material.dart';
 
-class LoginCard extends StatelessWidget {
+class LoginCard extends StatefulWidget {
   const LoginCard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
+  State<LoginCard> createState() => _LoginCardState();
+}
 
-    void onFormSave() {
-      if (formKey.currentState!.validate()) {
-        formKey.currentState!.save();
-      }
+class _LoginCardState extends State<LoginCard> {
+  final formKey = GlobalKey<FormState>();
+
+  bool onSignUp = false;
+
+  void onFormSave() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
     }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      // height: 300,
       width: double.infinity,
       padding: const EdgeInsets.all(16),
       margin: const EdgeInsets.symmetric(
@@ -29,9 +35,9 @@ class LoginCard extends StatelessWidget {
         key: formKey,
         child: Column(
           children: [
-            const Text(
-              "Login",
-              style: TextStyle(
+            Text(
+              onSignUp ? "Sign Up" : "Login",
+              style: const TextStyle(
                 fontSize: 18,
               ),
             ),
@@ -59,17 +65,20 @@ class LoginCard extends StatelessWidget {
 
             // forgot? login btn
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: onSignUp
+                  ? MainAxisAlignment.center
+                  : MainAxisAlignment.spaceBetween,
               children: [
-                TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Forgot Password',
-                    style: TextStyle(
-                      decoration: TextDecoration.underline,
+                if (!onSignUp)
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                      ),
                     ),
                   ),
-                ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     fixedSize: const Size(120, 30),
@@ -79,9 +88,9 @@ class LoginCard extends StatelessWidget {
                     backgroundColor: const Color(0xff292526),
                   ),
                   onPressed: onFormSave,
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
+                  child: Text(
+                    onSignUp ? "Sign Up" : 'Login',
+                    style: const TextStyle(
                       color: Colors.white,
                     ),
                   ),
@@ -90,10 +99,16 @@ class LoginCard extends StatelessWidget {
             ),
 
             TextButton(
-              onPressed: () {},
-              child: const Text(
-                'No Account? Sign Up',
-                style: TextStyle(
+              onPressed: () {
+                setState(() {
+                  onSignUp = !onSignUp;
+                });
+              },
+              child: Text(
+                onSignUp
+                    ? 'Alread have an account? login now!'
+                    : 'No Account? Sign Up!!',
+                style: const TextStyle(
                   fontSize: 16,
                   decoration: TextDecoration.underline,
                 ),
