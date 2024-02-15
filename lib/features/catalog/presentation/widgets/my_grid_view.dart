8b5a4f0b7f75defe6b78013474095681850a6b4b
lajor <rojal@gmail.com>
2024-02-15 14:00:30 +0545

    gridview shimmer

--- lib/features/catalog/presentation/widgets/my_grid_view.dart
+++ lib/features/catalog/presentation/widgets/my_grid_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../blocs/catalog bloc/catalog_bloc.dart';
 import 'my_grid_tile.dart';
@@ -24,13 +25,16 @@ class _MyGridViewState extends State<MyGridView> {
 
   @override
   Widget build(BuildContext context) {
+    final media = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: BlocBuilder<CatalogBloc, CatalogState>(
         builder: (context, state) {
           if (state is CatalogLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Shimmer.fromColors(
+              baseColor: Colors.grey.shade300,
+              highlightColor: Colors.grey.shade100,
+              child: GridViewShimmer(media: media),
             );
           } else if (state is CatalogLoaded) {
             final productList = state.productList;
@@ -57,6 +61,85 @@ class _MyGridViewState extends State<MyGridView> {
           }
         },
       ),
+    );
+  }
+}
+
+class GridViewShimmer extends StatelessWidget {
+  const GridViewShimmer({
+    super.key,
+    required this.media,
+  });
+
+  final Size media;
+
+  @override
+  Widget build(BuildContext context) {
+    return MasonryGridView.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 20,
+      crossAxisSpacing: 10,
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: media.height * 0.28,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(
+              height: media.height * 0.01,
+            ),
+            Container(
+              height: media.height * 0.025,
+              width: media.width * 0.25,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            SizedBox(
+              height: media.height * 0.01,
+            ),
+            Container(
+              height: media.height * 0.015,
+              width: media.width * 0.125,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            SizedBox(
+              height: media.height * 0.01,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: media.height * 0.025,
+                  width: media.width * 0.2,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                Container(
+                  height: media.height * 0.025,
+                  width: media.width * 0.125,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ],
+            )
+          ],
+        );
+      },
     );
   }
 }
